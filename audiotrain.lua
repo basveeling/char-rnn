6dcc3a11e3e9f16d07e00d4994eb1a0192ee0678
Bas Veeling <basveeling@gmail.com>
2015-06-09 20:23:48 +0200

    Combine eval and train activations

--- audiotrain.lua
+++ audiotrain.lua
@@ -46,18 +46,19 @@ cmd:option('-seq_length',40,'number of timesteps to unroll for')
 cmd:option('-batch_size',30,'number of sequences to train on in parallel')
 cmd:option('-max_epochs',30,'number of full passes through the training data')
 cmd:option('-grad_clip',5,'clip gradients at')
-cmd:option('-train_frac',.7,'fraction of data that goes into train set')
-cmd:option('-val_frac',.3,'fraction of data that goes into validation set')
+cmd:option('-train_frac',.5,'fraction of data that goes into train set')
+cmd:option('-val_frac',.5,'fraction of data that goes into validation set')
             -- note: test_frac will be computed as (1 - train_frac - val_frac)
 -- bookkeeping
 cmd:option('-seed',1234,'torch manual random number generator seed')
-cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
-cmd:option('-eval_val_every',200,'every how many iterations should we evaluate on validation data?')
+cmd:option('-print_every',5,'how many steps/minibatches between printing out the loss')
+cmd:option('-eval_val_every',20,'every how many iterations should we evaluate on validation data?')
 cmd:option('-checkpoint_dir', 'cv', 'output directory where checkpoints get written')
 cmd:option('-savefile','lstm','filename to autosave the checkpont to. Will be inside checkpoint_dir/')
 -- Audio options
 cmd:option('-cutoff_low',20,'lower cutoff the spectrogram')
-cmd:option('-cutoff_high',599,'upper cutoff the spectrogram')
+cmd:option('-cutoff_high',599,'upper cu toff the spectrogram')
+cmd:option('-save_activations_layer',10,'1:2*num_layers+1,  0 for disabled. save csv files of final layer neuron activations for use in sonic visualiser. even numbers are gated, uneven are ungated.')
 -- GPU/CPU
 cmd:option('-gpuid',-1,'which gpu to use. -1 = use CPU')
 cmd:text()
@@ -130,7 +131,7 @@ function eval_split(split_index, max_batches)
     local rnn_state = {[0] = init_state}
     for i = 1,n do -- iterate over batches in the split
         -- fetch a batch
-        local x, y, song_time = loader:next_batch(split_index)
+        local x, y, time_batch = loader:next_batch(split_index)
         if opt.gpuid >= 0 then -- ship the input arrays to GPU
             -- have to convert to float because integers can't be cuda()'d
             x = x:float():cuda()
@@ -139,7 +140,21 @@ function eval_split(split_index, max_batches)
         -- forward pass
         for t=1,opt.seq_length do
             clones.rnn[t]:evaluate() -- for dropout proper functioning
-            local lst = clones.rnn[t]:forward{x[{{}, t}], unpack(rnn_state[t-1])}
+            local lst = clones.rnn[t]:forward{x[{{}, t}], unpack(rnn_state[t-1]) }
+            -- Save activations further
+            if opt.save_activations_layer > 0 then
+                for b=0,opt.batch_size-1 do
+                    local activations_off = (t+b*opt.seq_length) + (loader.ntrain + loader.batch_ix[2]-1)*opt.batch_size*opt.seq_length
+                    for n=1,opt.rnn_size do
+                        -- add row to activations table at time activations_off for neuron n
+                        activations[n][activations_off] = {time_batch[{b+1, t}],
+                            clones.rnn[t].outnode.data.mapindex[opt.save_activations_layer].module.output[{b+1,{n}}][1],
+--                            loader.batch_song[loader.ntrain + loader.batch_ix[2]],
+                            'eval'
+                        }
+                    end
+                end
+            end
             rnn_state[t] = {}
             for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end
             local prediction = lst[#lst]
@@ -210,18 +225,19 @@ function feval(x)
     for t=1,opt.seq_length do
         clones.rnn[t]:training() -- make sure we are in correct mode (this is cheap, sets flag)
         local lst = clones.rnn[t]:forward{x[{{}, t}], unpack(rnn_state[t-1])}
-        -- clones.rnn[t].verbose = true
         -- for debugging: Save activations of final layer neurons per timestep
-        for b=0,opt.batch_size-1 do
-            local activations_off = (t+b*opt.seq_length) + (loader.batch_ix[1]-1)*opt.batch_size*opt.seq_length
-            for n=1,opt.rnn_size do
-                activations[n][activations_off] = {time_batch[{b+1, t}],
-                    clones.rnn[t].outnode.data.mapindex[opt.num_layers*2 - 1].module.output[{b+1,{n}}][1],
-                    loader.batch_song[loader.batch_ix[1]]
-                }
+        if opt.save_activations_layer > 0 then
+            for b=0,opt.batch_size-1 do
+                local activations_off = (t+b*opt.seq_length) + (loader.batch_ix[1]-1)*opt.batch_size*opt.seq_length
+                for n=1,opt.rnn_size do
+                    activations[n][activations_off] = {time_batch[{b+1, t}],
+                        clones.rnn[t].outnode.data.mapindex[opt.save_activations_layer].module.output[{b+1,{n}}][1],
+--                        loader.batch_song[loader.batch_ix[1]],
+                        'train'
+                    }
+                end
             end
         end
-
         rnn_state[t] = {}
         for i=1,#init_state do table.insert(rnn_state[t], lst[i]) end -- extract the state, without output
         predictions[t] = lst[#lst] -- last element is the prediction
@@ -266,22 +282,17 @@ local iterations_per_epoch = loader.ntrain
 local loss0 = nil
 collectgarbage()
 
--- Initialize activation logging TODO: make optional
-activations = {}
-for n=1, opt.rnn_size do activations[n] = {} end
-
+-- Initialize activation logging
+if opt.save_activations_layer then
+    activations = {}
+    for n=1, opt.rnn_size do activations[n] = {} end
+end
 for i = 1, iterations do
     local epoch = i / loader.ntrain
 
     local timer = torch.Timer()
     local _, loss = optim.rmsprop(feval, params, optim_state)
     local time = timer:time().real
-    -- Output final layer neuron activations TODO: make optional
-    if i % loader.ntrain == 0 then
-        for n=1,opt.rnn_size do
-            csvigo.save({verbose=false,path=string.format("/tmp/activations_neuron-%d_iteration-%d.csv",n,i),data=activations[n],headers=false})
-        end
-    end
 
     local train_loss = loss[1] -- the loss is inside a list, pop it
     train_losses[i] = train_loss
@@ -313,6 +324,18 @@ for i = 1, iterations do
         checkpoint.epoch = epoch
         checkpoint.vocab = loader.vocab_mapping
         torch.save(savefile, checkpoint)
+        -- Output final layer neuron activations
+        if opt.save_activations_layer > 0 then
+            for n=1,opt.rnn_size do
+                -- sort activations
+                local function compare_activation(a,b)
+                  return a[1] < b[1]
+                end
+                table.sort(activations[n],compare_activation)
+
+                csvigo.save({verbose=false,path=string.format("/tmp/activations_L%d_neuron-%d_iteration-%d.csv",opt.save_activations_layer,n,i),data=activations[n],headers=false})
+            end
+        end
     end
 
     if i % opt.print_every == 0 then
